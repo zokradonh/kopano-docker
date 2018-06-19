89b0351e0b4e11344c287fedb66eb09bf87c6753
Andre Zoledziowski <az@zok.xyz>
2018-06-19 15:07:01 +0200

    BugFix: Since version unknown, /etc/kopano is empty

--- server/start.sh
+++ server/start.sh
@@ -4,8 +4,7 @@ set -eu # unset variables are errors & non-zero return values exit the whole scr
 
 mkdir -p /kopano/data/attachments /var/run/kopano
 
-echo "Create /etc/kopano/ldap.cfg and prepare" | ts
-cp /usr/share/doc/kopano/example-config/ldap.cfg /etc/kopano/ldap.cfg
+echo "Create default configs and prepare" | ts
 sed -e "s|^\s*!include /usr/share/kopano/ldap.openldap.cfg|#!include /usr/sharekopano/ldap.openldap.cfg|" \
     -e "s|#!include /usr/share/kopano/ldap.active-directory.cfg|!include /usr/share/kopano/ldap.active-directory.cfg|" \
     -i /etc/kopano/ldap.cfg
