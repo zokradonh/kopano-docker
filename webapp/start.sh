9cd2dde6279decf824aa94bc76882512521a60be
Felix Bartels <felix@host-consultants.de>
2018-11-10 21:10:31 +0100

    remove z-push from webapp start script
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- webapp/start.sh
+++ webapp/start.sh
@@ -2,7 +2,7 @@
 
 # define default value for serverhostname and serverport if not passed into container
 KCCONF_SERVERHOSTNAME=${KCCONF_SERVERHOSTNAME:-127.0.0.1}
-KCCONF_SERVERPORT=${KCCONF_SERVERPORT:-237}
+KCCONF_SERVERPORT=${KCCONF_SERVERPORT:-236}
 ADDITIONAL_KOPANO_PACKAGES=${ADDITIONAL_KOPANO_PACKAGES:-""}
 
 set -eu # unset variables are errors & non-zero return values exit the whole script
@@ -31,27 +31,8 @@ sed \
     -e "s#define(\"INSECURE_COOKIES\",\s*.*)#define(\"INSECURE_COOKIES\", true)#" \
     -i /etc/kopano/webapp/config.php
 
-if [ "$KCCONF_SERVERHOSTNAME" == "127.0.0.1" ]; then
-	echo "Z-Push is using the default: connection"
-else
-	echo "Z-Push is using an ip connection"
-	sed -e "s#define([\"']MAPI_SERVER[\"'],\s*[\"']default:[\"'])#define('MAPI_SERVER', 'https://${KCCONF_SERVERHOSTNAME}:${KCCONF_SERVERPORT}/kopano')#" \
-	    -i /etc/z-push/kopano.conf.php
-fi
-
-echo "Configuring Z-Push for use behind a reverse proxy"
-sed -e "s#define([\"']USE_CUSTOM_REMOTE_IP_HEADER[\"'],\s*false)#define('USE_CUSTOM_REMOTE_IP_HEADER', true)#" \
-    -i /etc/z-push/z-push.conf.php
-
 echo "Ensure config ownership"
 chown -R www-data:www-data /run/sessions /tmp/webapp
-
-echo "Activate z-push log rerouting"
-touch /var/log/z-push/z-push.log
-touch /var/log/z-push/z-push-error.log
-chown www-data:www-data /var/log/z-push/z-push.log /var/log/z-push/z-push-error.log
-tail --pid=$$ -F --lines=0 -q /var/log/z-push/z-push.log &
-tail --pid=$$ -F --lines=0 -q /var/log/z-push/z-push-error.log &
 
 echo "Starting Apache"
 rm -f /run/apache2/apache2.pid
