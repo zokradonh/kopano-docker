323421039cec5703892722db9c8d4215a76208ca
Felix Bartels <felix@host-consultants.de>
2018-12-01 04:34:14 +0100

    fix review commit of @longsleep
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- kweb/wrapper.sh
+++ kweb/wrapper.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-if [ $EMAIL = "self_signed" ]; then
+if [ "$EMAIL" = "self_signed" ]; then
 	# do not use the '-host' option if using a self signed cert
-	kwebd caddy -conf /etc/kweb.cfg -agree
+	exec kwebd caddy -conf /etc/kweb.cfg -agree
 else
-	kwebd caddy -conf /etc/kweb.cfg -agree -host "$FQDN"
+	exec kwebd caddy -conf /etc/kweb.cfg -agree -host "$FQDN"
 fi
