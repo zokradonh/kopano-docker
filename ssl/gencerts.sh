bca28c31c68b713201f54dfdc639019019ef0f41
Felix Bartels <felix@host-consultants.de>
2018-12-06 17:31:51 +0100

    adapt gencerts for konnect certs
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- ssl/gencerts.sh
+++ ssl/gencerts.sh
@@ -17,4 +17,19 @@ for s in kserver kdagent kmonitor ksearch kspooler kwebapp
     openssl x509 -in /kopano/easypki/internalca/certs/$s.crt -pubkey -noout > /kopano/ssl/clients/$s-public.pem
 done
 
+# Konnect - create encryption key if not already present
+encckey="/kopano/ssl/konnectd-encryption.key"
+if [ ! -f $encckey ]; then
+	echo "creating new encryption key"
+	openssl rand -out /etc/kopano/konnectd-encryption.key 32
+fi
+
+# Konnec - create token signing key if not already present
+singkey="/kopano/ssl/konnectd-tokens-signing-key.pem"
+if [ ! -f $signkey ]; then
+	echo "creating new token signing key"
+	openssl genpkey -algorithm RSA -out $signkey -pkeyopt rsa_keygen_bits:4096
+fi
+
 ls -l /kopano/ssl/*.pem
+ls -l /kopano/ssl/*.key
