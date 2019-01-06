6488ec50743778b0aba1d1cafb76a238c7231bd8
Felix Bartels <felix@host-consultants.de>
2019-01-06 15:56:35 +0100

    remove sleep from konnect wrapper
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- konnect/wrapper.sh
+++ konnect/wrapper.sh
@@ -6,7 +6,6 @@ dockerize \
 	-wait file:///kopano/ssl/konnectd-tokens-signing-key.pem \
 	-wait file:///kopano/ssl/konnectd-encryption.key \
 	-timeout 360s
-sleep 1 # wait for openssl to finish writing the above files
 exec konnectd serve \
 	--signing-private-key=/kopano/ssl/konnectd-tokens-signing-key.pem \
 	--encryption-secret=/kopano/ssl/konnectd-encryption.key \
