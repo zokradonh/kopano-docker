d0d2c84ae875e6d4d3ac220ca47e49029eaf9870
Felix Bartels <felix@host-consultants.de>
2019-01-07 22:07:35 +0100

    really make ssl tasks atomic
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- ssl/start.sh
+++ ssl/start.sh
@@ -9,37 +9,38 @@ if [ ! -f /kopano/ssl/ca.pem ]; then
 
 	for s in kserver kdagent kmonitor ksearch kspooler kwebapp; do
 		easypki create --ca-name internalca --organizational-unit $s --expire 3650 $s
-		cp /kopano/easypki/internalca/keys/$s.key /tmp/$s.pem
-		cat /kopano/easypki/internalca/certs/$s.crt >> /tmp/$s.pem
-		openssl x509 -in /kopano/easypki/internalca/certs/$s.crt -pubkey -noout > /tmp/$s-public.pem
-		cp /tmp/$s.pem /kopano/ssl/$s.pem
-		cp /tmp/$s-public.pem /kopano/ssl/clients/$s-public.pem
+		cp /kopano/easypki/internalca/keys/$s.key /kopano/ssl/$s.pem.tmp
+		cat /kopano/easypki/internalca/certs/$s.crt >> /kopano/ssl/$s.pem.tmp
+		openssl x509 -in /kopano/easypki/internalca/certs/$s.crt -pubkey -noout >  /kopano/ssl/clients/$s-public.pem.tmp
+		mv /kopano/ssl/$s.pem.tmp /kopano/ssl/$s.pem
+		mv /kopano/ssl/clients/$s-public.pem.tmp /kopano/ssl/clients/$s-public.pem
 	done
 
-	cp /kopano/easypki/internalca/certs/internalca.crt /kopano/ssl/ca.pem
+	cp /kopano/easypki/internalca/certs/internalca.crt /kopano/ssl/ca.pem.tmp
+	mv /kopano/ssl/ca.pem.tmp /kopano/ssl/ca.pem
 fi
 
 # Konnect - create encryption key if not already present
 enckey="/kopano/ssl/konnectd-encryption.key"
 if [ ! -f $enckey ]; then
 	echo "creating new encryption key"
-	openssl rand -out /tmp/konnectd-encryption.key 32
-	cp /tmp/konnectd-encryption.key $enckey
+	openssl rand -out $enckey.tmp 32
+	mv $enckey.tmp $enckey
 fi
 
 # Konnect - create token signing key if not already present
 signkey="/kopano/ssl/konnectd-tokens-signing-key.pem"
 if [ ! -f $signkey ]; then
 	echo "creating new token signing key"
-	openssl genpkey -algorithm RSA -out /tmp/konnectd-tokens-signing-key.pem -pkeyopt rsa_keygen_bits:4096
-	cp /tmp/konnectd-tokens-signing-key.pem $signkey
+	openssl genpkey -algorithm RSA -out $signkey.tmp -pkeyopt rsa_keygen_bits:4096
+	mv $signkey.tmp $signkey
 fi
 
 # Kapi
 secretkey="/kopano/ssl/kapid-pubs-secret.key"
 if [ ! -f $secretkey ]; then
-	openssl rand -out /tmp/kapid-pubs-secret.key -hex 64
-	cp /tmp/kapid-pubs-secret.key $secretkey
+	openssl rand -out $secretkey.tmp -hex 64
+	mv $secretkey.tmp $secretkey
 fi
 
 ls -l /kopano/ssl/*.pem
