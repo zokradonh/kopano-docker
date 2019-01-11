3b043334d53c14737fdcdded479be3b287604fb2
Christian Günther <christian.guenther@vireso.de>
2019-01-11 12:16:46 +0100

    support disables ssl on caddy, configured via .env

--- web/wrapper.sh
+++ web/wrapper.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ "$EMAIL" = "self_signed" ]; then
+if [ "$EMAIL" = "self_signed" ] || [ "$EMAIL" = "off" ]; then
 	# do not use the '-host' option if using a self signed cert
 	exec kwebd caddy -conf /etc/kweb.cfg -agree
 else
