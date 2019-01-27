f79b0e6eaa6b398f25c7cfa8d21964e07c4b1977
Felix Bartels <felix@host-consultants.de>
2019-01-27 21:59:49 +0100

    add compose to the build image
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- build/run.sh
+++ build/run.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+if [[ $EUID -ne 0 ]]; then
+	echo "This script must be run as root"
+	exit 1
+fi
 docker run \
 	--rm -it \
-	-u $(id -u ${USER}):$(id -g ${USER}) \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v ${PWD}/..:/kopano-docker/ \
 	$(docker build -q .)
