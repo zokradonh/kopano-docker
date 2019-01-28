9b452fb5a161cb827ae70efe4036f8a0e419bb8f
Felix Bartels <felix@host-consultants.de>
2019-01-28 08:06:27 +0100

    fine tuning for build container
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- build/run.sh
+++ build/run.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-if [[ $EUID -ne 0 ]]; then
-	echo "This script must be run as root"
-	exit 1
+if [ ! $(id -u) -eq 0 ]; then
+	echo "This script may need to be run as root to be able to use docker/docker-compose through it."
 fi
 docker run \
 	--rm -it \
+	-u $(id -u ${USER}):$(id -g ${USER}) \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v ${PWD}/..:/kopano-docker/ \
 	$(docker build -q .)
-
