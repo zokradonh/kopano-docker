57e6fc38edb64d4b5c565bd22694e12cfdaf9f54
Felix Bartels <felix@host-consultants.de>
2019-03-11 11:14:02 +0100

    upstream docker container has been updated to alpine 3.9
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- konnect/wrapper.sh
+++ konnect/wrapper.sh
@@ -9,10 +9,9 @@ cd /kopano/ssl/
 konnectd utils jwk-from-pem --use sig /kopano/ssl/meet-kwmserver.pem > /tmp/meet.ywk
 CONFIG_JSON=/etc/kopano/konnectd-identifier-registration.yaml
 yq -y '.clients += [{"id": "meet", "name": "Kopano Meet", "application_type": "web", "trusted": true, "redirect_uris": ["https://\"$FQDN\"/meet/"], "trusted_scopes": ["konnect/guestok", "kopano/kwm"], "jwks": {"keys": []},"request_object_signing_alg": "ES256"}]' \
-$CONFIG_JSON > $CONFIG_JSON.tmp
-mv $CONFIG_JSON.tmp $CONFIG_JSON
+$CONFIG_JSON | sponge $CONFIG_JSON
+
 cat $CONFIG_JSON
-# TODO replace above with sponge once base image is updated to alpine 3.9
 # TODO add actual keys from /tmp/meet.ywk
 
 dockerize \
