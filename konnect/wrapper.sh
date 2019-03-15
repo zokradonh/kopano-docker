a0a1b9b728bea4a202ef2deb4d482e9c46162a51
Felix Bartels <felix@host-consultants.de>
2019-03-15 15:53:45 +0100

    key generation works!
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- konnect/wrapper.sh
+++ konnect/wrapper.sh
@@ -7,13 +7,8 @@ dockerize \
 	-timeout 360s
 cd /kopano/ssl/
 konnectd utils jwk-from-pem --use sig /kopano/ssl/meet-kwmserver.pem > /tmp/jwk-meet.json
-konnectd utils jwk-from-pem --yaml --use sig /kopano/ssl/meet-kwmserver.pem > /tmp/jwk-meet.yaml
 CONFIG_JSON=/etc/kopano/konnectd-identifier-registration.yaml
-#yq -y ".clients |= [{\"id\": \"meet\", \"name\": \"Kopano Meet\", \"application_type\": \"web\", \"trusted\": true, \"redirect_uris\": [\"https://$FQDN/meet/\"], \"trusted_scopes\": [\"konnect/guestok\", \"kopano/kwm\"], \"jwks\": {\"keys\": },\"request_object_signing_alg\": \"ES256\"}]" \
-#$CONFIG_JSON | sponge $CONFIG_JSON
-
-# insert jwks /tmp/meet.ywk https://unix.stackexchange.com/questions/460985/jq-add-objects-from-file-into-json-array
-#yq ' .clients | to_entries | .[].value | .jwks' $CONFIG_JSON
+yq -y ".clients |= [{\"id\": \"meet\", \"name\": \"Kopano Meet\", \"application_type\": \"web\", \"trusted\": true, \"redirect_uris\": [\"https://$FQDN/meet/\"], \"trusted_scopes\": [\"konnect/guestok\", \"kopano/kwm\"], \"jwks\": {\"keys\": [{\"kty\": $(jq .kty /tmp/jwk-meet.json), \"use\": $(jq .use /tmp/jwk-meet.json), \"crv\": $(jq .crv /tmp/jwk-meet.json), \"d\": $(jq .d /tmp/jwk-meet.json), \"kid\": $(jq .kid /tmp/jwk-meet.json), \"x\": $(jq .x /tmp/jwk-meet.json), \"y\": $(jq .y /tmp/jwk-meet.json)}]},\"request_object_signing_alg\": \"ES256\"}]" $CONFIG_JSON | sponge $CONFIG_JSON
 
 cat $CONFIG_JSON
 
