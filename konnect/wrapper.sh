cc4c685d6d7009fa926b561499696c55824d3538
Felix Bartels <felix@host-consultants.de>
2019-03-15 16:02:08 +0100

    remove debug output
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- konnect/wrapper.sh
+++ konnect/wrapper.sh
@@ -6,11 +6,10 @@ dockerize \
 	-wait file:///kopano/ssl/meet-kwmserver.pem \
 	-timeout 360s
 cd /kopano/ssl/
+
 konnectd utils jwk-from-pem --use sig /kopano/ssl/meet-kwmserver.pem > /tmp/jwk-meet.json
 CONFIG_JSON=/etc/kopano/konnectd-identifier-registration.yaml
 yq -y ".clients |= [{\"id\": \"meet\", \"name\": \"Kopano Meet\", \"application_type\": \"web\", \"trusted\": true, \"redirect_uris\": [\"https://$FQDN/meet/\"], \"trusted_scopes\": [\"konnect/guestok\", \"kopano/kwm\"], \"jwks\": {\"keys\": [{\"kty\": $(jq .kty /tmp/jwk-meet.json), \"use\": $(jq .use /tmp/jwk-meet.json), \"crv\": $(jq .crv /tmp/jwk-meet.json), \"d\": $(jq .d /tmp/jwk-meet.json), \"kid\": $(jq .kid /tmp/jwk-meet.json), \"x\": $(jq .x /tmp/jwk-meet.json), \"y\": $(jq .y /tmp/jwk-meet.json)}]},\"request_object_signing_alg\": \"ES256\"}]" $CONFIG_JSON | sponge $CONFIG_JSON
-
-cat $CONFIG_JSON
 
 dockerize \
 	-wait file:///kopano/ssl/konnectd-tokens-signing-key.pem \
