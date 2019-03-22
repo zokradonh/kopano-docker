687b8d03841bd2a44cb266d9512b500fcb8452f2
Felix Bartels <felix@host-consultants.de>
2019-03-23 00:10:48 +0100

    do not wait for mailserver startup
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- tests/startup-test/test.sh
+++ tests/startup-test/test.sh
@@ -5,5 +5,4 @@ exec dockerize \
 	-wait tcp://kopano_server:236 \
 	-wait file://var/run/kopano/server.sock \
 	-wait tcp://"${KCCONF_SERVER_MYSQL_HOST}":3306 \
-	-wait tcp://mail:25 \
 	-timeout 360s
