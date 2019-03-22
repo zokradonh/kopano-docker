83fedde584dd470bc5a737f27afc5f61387e3241
Felix Bartels <felix@host-consultants.de>
2019-03-23 00:19:55 +0100

    add kopano ssl port
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- tests/startup-test/test.sh
+++ tests/startup-test/test.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+# waits for key events in various containers
+# e.g. kopano_server:236 signals succesful start of kopano-server process
 exec dockerize \
 	-wait file://var/run/kopano/grapi/notify.sock \
 	-wait http://kopano_konnect:8777/.well-known/openid-configuration \
 	-wait tcp://kopano_server:236 \
+	-wait tcp://kopano_server:237 \
 	-wait tcp://kopano_gateway:143 \
 	-wait tcp://kopano_ical:8080 \
 	-wait tcp://kopano_webapp:80 \
