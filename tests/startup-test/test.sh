f1d5fb9532d9aa5e535c830279e197d0b5611626
Felix Bartels <felix@host-consultants.de>
2019-03-23 07:58:08 +0100

    add more tests and sort
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- tests/startup-test/test.sh
+++ tests/startup-test/test.sh
@@ -3,13 +3,16 @@
 # e.g. kopano_server:236 signals succesful start of kopano-server process
 exec dockerize \
 	-wait file://var/run/kopano/grapi/notify.sock \
+	-wait file://var/run/kopano/server.sock \
 	-wait http://kopano_konnect:8777/.well-known/openid-configuration \
-	-wait tcp://kopano_server:236 \
-	-wait tcp://kopano_server:237 \
+	-wait tco://kopano_konnect:8778 \
+	-wait tcp://"${KCCONF_SERVER_MYSQL_HOST}":3306 \
+	-wait tcp://kopano_dagent:2003 \
 	-wait tcp://kopano_gateway:143 \
 	-wait tcp://kopano_ical:8080 \
+	-wait tcp://kopano_server:236 \
+	-wait tcp://kopano_server:237 \
+	-wait tcp://kopano_web:2015 \
 	-wait tcp://kopano_webapp:80 \
 	-wait tcp://kopano_zpush:80 \
-	-wait file://var/run/kopano/server.sock \
-	-wait tcp://"${KCCONF_SERVER_MYSQL_HOST}":3306 \
 	-timeout 120s
