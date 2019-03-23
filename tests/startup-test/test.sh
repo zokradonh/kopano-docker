b98ecf234230faf5314f7c86ed352f1bc1b1af75
Felix Bartels <felix@host-consultants.de>
2019-03-23 16:58:53 +0100

    fix tests
    (why did it not fail before?)
    
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- tests/startup-test/test.sh
+++ tests/startup-test/test.sh
@@ -9,10 +9,10 @@ exec dockerize \
 	-wait tcp://kopano_dagent:2003 \
 	-wait tcp://kopano_gateway:143 \
 	-wait tcp://kopano_ical:8080 \
-	-wait tcp://kopano_konnect:8778 \
+	-wait tcp://kopano_kwmserver:8778 \
 	-wait tcp://kopano_server:236 \
 	-wait tcp://kopano_server:237 \
-	-wait tcp://kopano_web:2015 \
+	-wait tcp://web:2015 \
 	-wait tcp://kopano_webapp:80 \
 	-wait tcp://kopano_zpush:80 \
 	-timeout 120s
