18b85a7ebb1b47f926580eae227d019681316ede
Felix Bartels <felix@host-consultants.de>
2019-08-04 21:59:51 +0200

    the fqdn needs to be part of kweb.cfg
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- web/wrapper.sh
+++ web/wrapper.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+dockerize -template /etc/kweb.cfg.tpml:/etc/kweb.cfg
+
 if [ "$EMAIL" = "self_signed" ] || [ "$EMAIL" = "off" ]; then
 	# do not use the '-host' option if using a self signed cert
 	exec kwebd caddy -conf /etc/kweb.cfg -agree
