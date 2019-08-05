0f1287d3e743908efd1d5c9faaae73c9c6e50bfa
Felix Bartels <felix@host-consultants.de>
2019-08-05 12:22:25 +0200

    do not need dockerize, kweb can du env replacement on its own
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- web/wrapper.sh
+++ web/wrapper.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-dockerize -template /etc/kweb.cfg.tpml:/etc/kweb.cfg
-
 if [ "$EMAIL" = "self_signed" ] || [ "$EMAIL" = "off" ]; then
 	# do not use the '-host' option if using a self signed cert
 	exec kwebd caddy -conf /etc/kweb.cfg -agree
