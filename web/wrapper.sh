883fbaac5ac4f43acc1fa03769b713006e620f51
Felix Bartels <felix@host-consultants.de>
2019-08-07 13:21:30 +0200

    final changes
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- web/wrapper.sh
+++ web/wrapper.sh
@@ -2,9 +2,4 @@
 
 set -e
 
-if [ "$EMAIL" = "self_signed" ] || [ "$EMAIL" = "off" ]; then
-	# do not use the '-host' option if using a self signed cert
-	exec kwebd caddy -conf /etc/kweb.cfg -agree
-else
-	exec kwebd caddy -conf /etc/kweb.cfg -agree -host "$FQDN"
-fi
+exec kwebd caddy -conf /etc/kweb.cfg -agree
