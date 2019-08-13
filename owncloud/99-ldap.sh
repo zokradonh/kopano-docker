09b98220282c415379ec982dbfd538c3fd87437a
Felix Bartels <felix@host-consultants.de>
2019-08-13 07:38:09 +0200

    owncloud config works now
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- owncloud/99-ldap.sh
+++ owncloud/99-ldap.sh
@@ -2,6 +2,8 @@
 
 echo "Configuring LDAP for kopano-docker"
 
+set -x
+
 occ app:enable user_ldap
 occ ldap:show-config
 
@@ -9,15 +11,20 @@ if [[ "$(occ ldap:show-config)" == "" ]]; then
 	su -c "php occ ldap:create-empty-config" www-data
 fi
 
-occ ldap:set-config s01 ldapHost ${LDAP_SERVER}
+ldapHost=${LDAP_SERVER%:*}
+ldapPort=${LDAP_SERVER##*:}
+
+occ ldap:set-config s01 ldapHost ${ldapHost}
+occ ldap:set-config s01 ldapPort ${ldapPort}
 occ ldap:set-config s01 ldapAgentName ${LDAP_BIND_DN}
 occ ldap:set-config s01 ldapAgentPassword ${LDAP_BIND_PW}
 occ ldap:set-config s01 ldapBase ${LDAP_SEARCH_BASE}
-occ ldap:set-config s01 ldapUserFilter ${LDAP_QUERY_FILTER_USER}
-occ ldap:set-config s01 ldapGroupFilter ${LDAP_QUERY_FILTER_GROUP}
+occ ldap:set-config s01 ldapUserFilter "(|(objectclass=kopano-user))"
+occ ldap:set-config s01 ldapLoginFilter "(&(|(objectclass=kopano-user))(uid=%uid))"
+occ ldap:set-config s01 ldapGroupFilter "(&(|(objectclass=kopano-group)))"
 occ ldap:set-config s01 ldapConfigurationActive 1
 
-/usr/bin/occ user:sync -m disable "OCA\User_LDAP\User_Proxy" 
+/usr/bin/occ user:sync -m disable "OCA\User_LDAP\User_Proxy"
 
 cat << EOF >| /etc/cron.d/sync
 */10  *  *  *  * root /usr/bin/occ user:sync -m disable 'OCA\User_LDAP\User_Proxy'
