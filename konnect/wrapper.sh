539c28b97d0a6a28bf920b4d7420aea4c849e67b
Felix Bartels <felix@host-consultants.de>
2019-08-19 22:24:45 +0200

    separate konnectd from dockerize
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- konnect/wrapper.sh
+++ konnect/wrapper.sh
@@ -29,8 +29,8 @@ fi
 dockerize \
 	-wait file:///kopano/ssl/konnectd-tokens-signing-key.pem \
 	-wait file:///kopano/ssl/konnectd-encryption.key \
-	-timeout 360s \
-	konnectd serve \
+	-timeout 360s
+konnectd serve \
 	--signing-private-key=/kopano/ssl/konnectd-tokens-signing-key.pem \
 	--encryption-secret=/kopano/ssl/konnectd-encryption.key \
 	--iss=https://"$FQDN" \
