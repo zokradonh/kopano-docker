9b46990e8140834b0f6d67fb58e478ac21a2a618
Felix Bartels <felix@host-consultants.de>
2019-08-24 21:14:07 +0200

    restructure goss files
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- core/healthcheck.sh
+++ core/healthcheck.sh
@@ -4,7 +4,7 @@ set -e
 
 case "$SERVICE_TO_START" in
 server|dagent)
-	goss -g /goss/goss_$SERVICE_TO_START.yaml validate --format json_oneline
+	goss -g /goss/$SERVICE_TO_START/goss.yaml validate --format json_oneline
 	;;
 *)
 	echo "This service still needs a proper check"
