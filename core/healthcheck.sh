92f1b30e06d1cf2d0655d65aa8c09f697c81668a
Felix Bartels <felix@host-consultants.de>
2019-08-24 21:44:48 +0200

    reorg tests
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- core/healthcheck.sh
+++ core/healthcheck.sh
@@ -4,7 +4,7 @@ set -e
 
 case "$SERVICE_TO_START" in
 server|dagent)
-	goss -g /goss/$SERVICE_TO_START/goss.yaml validate --format json_oneline
+	goss -g /kopano/goss/$SERVICE_TO_START/goss.yaml validate --format json_oneline
 	;;
 *)
 	echo "This service still needs a proper check"
