e2383bd7e821049f3d92bc4ae7b7abfa57688af3
Felix Bartels <felix@host-consultants.de>
2019-08-29 12:54:25 +0200

    add on the fly debugging to meet container
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- meet/start-service.sh
+++ meet/start-service.sh
@@ -3,6 +3,7 @@
 ADDITIONAL_KOPANO_PACKAGES=${ADDITIONAL_KOPANO_PACKAGES:-""}
 
 set -eu # unset variables are errors & non-zero return values exit the whole script
+[ "$DEBUG" ] && set -x
 
 if [ ! -e /kopano/"$SERVICE_TO_START".py ]; then
 	echo "Invalid service specified: $SERVICE_TO_START" | ts
