60ed693cbf3cebf444bca966173f32067879ad69
Felix Bartels <felix@host-consultants.de>
2019-09-10 20:40:20 +0200

    fix linting
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- database/create-additional-databases.sh
+++ database/create-additional-databases.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
+mysql=${mysql:?}
+
 function create_user_and_database() {
 	local database=$1
 	echo "  Creating database '$database'"
@@ -11,8 +13,8 @@ function create_user_and_database() {
 
 if [ -n "$MYSQL_ADDITIONAL_DATABASES" ]; then
 	echo "Multiple database creation requested: $MYSQL_ADDITIONAL_DATABASES"
-	for db in $(echo $MYSQL_ADDITIONAL_DATABASES | tr ',' ' '); do
-		create_user_and_database $db
+	for db in $(echo "$MYSQL_ADDITIONAL_DATABASES" | tr ',' ' '); do
+		create_user_and_database "$db"
 	done
 	echo "Additional databases created"
 fi
