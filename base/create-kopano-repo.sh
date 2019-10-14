ab8d008f468540d73556d2eaba90ad4218505cf8
Felix Bartels <felix@host-consultants.de>
2019-10-14 11:17:21 +0200

    fix version for pre-final packages
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- base/create-kopano-repo.sh
+++ base/create-kopano-repo.sh
@@ -6,7 +6,7 @@ set -eu
 function urldecode { : "${*//+/ }"; echo -e "${_//%/\\x}"; }
 
 function version_from_filename {
-	echo "$1" | awk -F"-" '{print $2}'
+	echo $(basename "$1") | awk -F"-" '{print $2}'
 }
 
 function h5ai_query {
