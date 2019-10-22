421986a68f859cc49bc7102d69b597d9b4dbb413
Felix Bartels <felix@host-consultants.de>
2019-10-22 09:43:47 +0200

    define ical_listen for kopano-ical
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- tests/startup-test/test.sh
+++ tests/startup-test/test.sh
@@ -11,13 +11,13 @@ dockerize \
 	-wait tcp://"${KCCONF_SERVER_MYSQL_HOST}":3306 \
 	-wait tcp://kopano_dagent:2003 \
 	-wait tcp://kopano_gateway:143 \
-	-wait tcp://kopano_ical:8080 \
+	-wait tco://kopano_ical:8080 \
 	-wait tcp://kopano_kwmserver:8778 \
-	-wait tcp://kopano_meet:9080 \
+	-wait http://kopano_meet:9080/meet \
 	-wait tcp://kopano_server:236 \
 	-wait tcp://kopano_server:237 \
 	-wait tcp://web:2015 \
-	-wait tcp://kopano_webapp:9080 \
+	-wait http://kopano_webapp:9080/webapp \
 	-wait tcp://kopano_zpush:80 \
 	-timeout 120s
 
