a827ef65b7b618b1662d63810a0d00fd68973b2b
Felix Bartels <1257835+fbartels@users.noreply.github.com>
2019-10-29 12:16:39 +0100

    Make sure that services listen globally (#273)
    they need to be made available through reverse proxy and port forwarding anyways
    changed in https://jira.kopano.io/browse/KC-1616
    
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- core/defaultconfigs/server.py
+++ core/defaultconfigs/server.py
@@ -9,8 +9,8 @@ kcconf.configkopano({
         'log_level': "3",
         'attachment_path': "/kopano/data/attachments/",
         'user_plugin': "ldap",
-        'server_listen': "*:236",
-        'server_listen_tls': "*:237",
+        'server_listen': "0.0.0.0:236",
+        'server_listen_tls': "0.0.0.0:237",
         'sync_gab_realtime': "no",
         'softdelete_lifetime': "0",
         'kcoidc_initialize_timeout': "360"
