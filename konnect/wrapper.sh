b7677bab00e670473f0557b50df28cdf4ee515c3
Felix Bartels <felix@host-consultants.de>
2019-11-08 10:31:31 +0100

    ci: committing changes for konnect

--- konnect/wrapper.sh
+++ konnect/wrapper.sh
@@ -39,7 +39,6 @@ if [ "${external_oidc_provider:-}" = "yes" ]; then
 	echo "Patching identifier registration for external OIDC provider"
 	CONFIG_JSON=/etc/kopano/konnectd-identifier-registration.yaml
 	#echo "{authorities: [{name: ucs-konnect, default: yes, iss: ${external_oidc_url:-}, client_id: kopano-meet, client_secret: ${external_oidc_clientsecret:-}, authority_type: oidc, response_type: id_token, scopes: [openid, profile, email]}]}" >> $CONFIG_JSON
-	#yq -y . $CONFIG_JSON | sponge /kopano/ssl/konnectd-identifier-registration.yaml
 cat <<EOT >> $CONFIG_JSON
 authorities:
   - name: ucs-konnect
@@ -54,6 +53,7 @@ authorities:
       - profile
       - email
 EOT
+	yq -y . $CONFIG_JSON | sponge /kopano/ssl/konnectd-identifier-registration.yaml
 fi
 
 # source additional configuration from Konnect cfg (potentially overwrites env vars)
