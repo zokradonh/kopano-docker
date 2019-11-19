753dc21eae9b85e78fac8824d0fa2449692e7999
Thomas Müller <thomas.mueller@tmit.eu>
2019-11-19 08:26:49 +0100

    Configuration file for OpenIdConnect integration is now generated on … (#5)
    * Configuration file for OpenIdConnect integration is now generated on container startup
    
    * Add a startup dependency to owncloud phoenix
    
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- owncloud/99-oidc.sh
+++ owncloud/99-oidc.sh
@@ -6,4 +6,25 @@ set -x
 
 occ app:enable openidconnect
 
+TODAY=$(date)
+cat <<EOF >/mnt/data/config/konnectd.config.php
+// Generated on $TODAY
+<?php
+\$CONFIG = [
+	'loglevel' => 0,
+	'debug' => true,
+	'openid-connect' => [
+		'provider-url' => 'https://$OWNCLOUD_DOMAIN',
+		'client-id' => 'ownCloud',
+		'client-secret' => 'ownCloud',
+		'loginButtonName' => 'kopano',
+		'autoRedirectOnLoginPage' => false,
+		'redirect-url' => 'https://$OWNCLOUD_DOMAIN/owncloud/index.php/apps/openidconnect/redirect',
+		'mode' => 'email',
+		'search-attribute' => 'email',
+		'use-token-introspection-endpoint' => false
+	],
+];
+EOF
+
 true
