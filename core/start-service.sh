3fc02d7003345e043422b909c2e7f65040c0ae4d
Felix Bartels <felix@host-consultants.de>
2019-12-22 18:29:05 +0100

    add action for eclint
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- core/start-service.sh
+++ core/start-service.sh
@@ -24,7 +24,6 @@ ADDITIONAL_KOPANO_PACKAGES=$(echo "$ADDITIONAL_KOPANO_PACKAGES" | tr -d '"')
 [ -n "${ADDITIONAL_KOPANO_PACKAGES// }" ] && apt update
 [ -n "${ADDITIONAL_KOPANO_PACKAGES// }" ] && for installpkg in $ADDITIONAL_KOPANO_PACKAGES; do
 	# shellcheck disable=SC2016 disable=SC2086
-	# test
 	if [ "$(dpkg-query -W -f='${Status}' $installpkg 2>/dev/null | grep -c 'ok installed')" -eq 0 ]; then
 		DEBIAN_FRONTEND=noninteractive apt --assume-yes --no-upgrade install "$installpkg"
 	else
