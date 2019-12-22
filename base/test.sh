f1bccd8e2072a9b2c0a172fbb7530a694acce196
Felix Bartels <felix@host-consultants.de>
2019-12-22 12:52:33 +0100

    linting
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- base/test.sh
+++ base/test.sh
@@ -8,13 +8,13 @@ WORK_DIR=$(mktemp -d)
 component=${1:-core}
 
 function cleanup {
-  rm -rf "$WORK_DIR"
-  echo "Deleted temp working directory $WORK_DIR"
+	rm -rf "$WORK_DIR"
+	echo "Deleted temp working directory $WORK_DIR"
 }
 
 trap cleanup EXIT
 
 cd "$WORK_DIR"
 # shellcheck source=base/create-kopano-repo.sh
-. $DIR/create-kopano-repo.sh
+. "$DIR"/create-kopano-repo.sh
 dl_and_package_community "$component"
