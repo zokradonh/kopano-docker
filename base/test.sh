ef52e5f94a4a6c948e1477b91386537bd29b31ae
Felix Bartels <felix@host-consultants.de>
2019-12-22 12:34:29 +0100

    add tests for downloading archive from kopano download server add some makefile defaults
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

---
+++ base/test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DEBUG=true
+WORK_DIR=$(mktemp -d)
+component=${1:-core}
+
+function cleanup {
+  rm -rf "$WORK_DIR"
+  echo "Deleted temp working directory $WORK_DIR"
+}
+
+trap cleanup EXIT
+
+cd "$WORK_DIR"
+# shellcheck source=base/create-kopano-repo.sh
+. $DIR/create-kopano-repo.sh
+dl_and_package_community "$component"
