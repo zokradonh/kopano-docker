5faba12c2b4237aed3dcafdefb5e7cacca3293dd
Felix Bartels <felix@host-consultants.de>
2020-02-03 17:02:27 +0100

    linting fixes
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- core/bin/kopano-create-missing-stores.sh
+++ core/bin/kopano-create-missing-stores.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for kuser in $(kopano-storeadm -O | grep -A999999999 "Entities without stores:" | tail -n +4 | awk '{print $2}'); do
-	kopano-storeadm -n $kuser -C
+	kopano-storeadm -n "$kuser" -C
 done
