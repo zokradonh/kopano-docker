5f10e982a685ea83e2f5965390eabc59f82f2db8
Felix Bartels <1257835+fbartels@users.noreply.github.com>
2020-02-07 16:03:05 +0100

    relax machine-id check for now (#336)
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- kdav/start.sh
+++ kdav/start.sh
@@ -48,9 +48,9 @@ echo "Ensure config ownership"
 chown -R www-data:www-data /run/sessions
 
 # services need to be aware of the machine-id
-dockerize \
-	-wait file:///etc/machine-id \
-	-wait file:///var/lib/dbus/machine-id
+#dockerize \
+#	-wait file:///etc/machine-id \
+#	-wait file:///var/lib/dbus/machine-id
 
 touch /var/log/kdav/kdav.log
 chown www-data:www-data /var/log/kdav/kdav.log
