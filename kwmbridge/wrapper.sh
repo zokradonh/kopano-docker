50c7a7524aca627f402f1f0b92da8332e7e0679f
Felix Bartels <felix@host-consultants.de>
2020-06-09 15:13:47 +0200

    add new config options for kwmserver and kwmbridge
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- kwmbridge/wrapper.sh
+++ kwmbridge/wrapper.sh
@@ -7,9 +7,38 @@ if [ -n "${log_level:-}" ]; then
 	set -- "$@" --log-level="$log_level"
 fi
 
-# shellcheck disable=SC2154
 if [ -n "${oidc_issuer_identifier:-}" ]; then
 	set -- "$@" --iss="$oidc_issuer_identifier"
+fi
+
+if [ -n "${kwm_server_urls:-}" ]; then
+	for url in $kwm_server_urls; do
+		set -- "$@" --kwmserver-url="$url"
+	done
+fi
+
+if [ -n "${ice_interfaces:-}" ]; then
+	for ice_if in $ice_interfaces; do
+		set -- "$@" --use-ice-if="$ice_if"
+	done
+fi
+
+if [ -n "${ice_network_types:-}" ]; then
+	for ice_network_type in $ice_network_types; do
+		set -- "$@" --use-ice-network-type="$ice_network_type"
+	done
+fi
+
+if [ -n "${ice_udp_port_range:-}" ]; then
+	set -- "$@" --use-ice-udp-port-range="$ice_udp_port_range"
+fi
+
+if [ "${with_metrics:-}" = "yes" ]; then
+	set -- "$@" --with-metrics
+fi
+
+if [ "${metrics_listen:-}" ]; then
+	set -- "$@" --metrics-listen="$metrics_listen"
 fi
 
 if [ "$INSECURE" = "yes" ]; then
@@ -32,5 +61,5 @@ dockerize \
 	-wait file:///etc/machine-id \
 	-wait file:///var/lib/dbus/machine-id
 
-exec sh -x /usr/local/bin/docker-entrypoint.sh serve \
+exec kwmbridged serve \
 	"$@"
